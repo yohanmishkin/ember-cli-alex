f236cb3bf3561a99972c806482abfc050526f2fa
Yohan Mishkin <yohanmishkin@protonmail.com>
2018-03-11 00:12:44 -0500

    ember-cli-alex test WIP

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
+  name: 'ember-cli-alex',
 
+  init() {
+    console.log('init!!!!');
+  },
+
+  included: function(app) {
+    console.log('included!!!!!!');
+  },
+
+  lintTree: function(type, tree) {
+    console.log('heyyyyyyyyy');
+    console.log(type);
+  }
 };
--- node-tests/test.js
+++ node-tests/test.js
@@ -2,27 +2,19 @@
 var assert = require('assert');
 var exec = require('child_process').exec;
 var Promise = require('es6-promise').Promise;
+var expect = require('chai').expect;
 
 describe('ember-cli-alex', function() {
 
-  it('passes if alex.js tests pass', function() {
-    return emberTest().then(function(result) {
-      assert.ifError(result.error);
-    //   expect(result.stdout.match(/[^\r\n]+/g))
-    //     .to.contain('ok 1 PhantomJS 2.1 - ESLint | app.js: should pass ESLint')
-    //     .to.contain('ok 2 PhantomJS 2.1 - ESLint | controllers/thing.js: should pass ESLint')
-    //     .to.contain('ok 3 PhantomJS 2.1 - ESLint | helpers/destroy-app.js: should pass ESLint')
-    //     .to.contain('ok 4 PhantomJS 2.1 - ESLint | helpers/module-for-acceptance.js: should pass ESLint')
-    //     .to.contain('ok 5 PhantomJS 2.1 - ESLint | helpers/resolver.js: should pass ESLint')
-    //     .to.contain('ok 6 PhantomJS 2.1 - ESLint | helpers/start-app.js: should pass ESLint')
-    //     .to.contain('ok 7 PhantomJS 2.1 - ESLint | models/thing.js: should pass ESLint')
-    //     .to.contain('ok 8 PhantomJS 2.1 - ESLint | resolver.js: should pass ESLint')
-    //     .to.contain('ok 9 PhantomJS 2.1 - ESLint | router.js: should pass ESLint')
-    //     .to.contain('ok 10 PhantomJS 2.1 - ESLint | test-helper.js: should pass ESLint')
-    //     .to.not.contain('not ok 11 PhantomJS 2.1 - ESLint | unused.js: should pass ESLint');
-    // })
-    });
+  this.timeout(60000);
 
+  it('passes if alex.js tests pass', function() {
+    return emberTest().then((result) => {
+      expect(result.error).to.not.exist;
+      expect(result.stdout.match(/[^\r\n]+/g))
+        .to.contain('ok 1 PhantomJS 2.1 - Alex.js | application.hbs: should pass Alex.js')
+        .to.not.contain('not ok 11 PhantomJS 2.1 - ESLint | unused.js: should pass ESLint');
+    });
   });
 
 });
