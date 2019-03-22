8603b1b572fe1e7baf6ada65b6d86a4dd13fc0de
Yohan Mishkin <yohanmishkin@protonmail.com>
2019-03-22 18:40:52 -0400

    Fixed ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,26 +12,10 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.12',
+          name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              'ember-source': '~2.12.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0'
+              'ember-source': '~3.4.0'
             }
           }
         },
